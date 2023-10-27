0507851c9e6a445743198c06e2e35ce344e579a6
GwerhDev <gwerh.dev@gmail.com>
2023-10-27 11:31:45 -0300

    adding rate, gallery & category models

--- src/models/Product.js
+++ src/models/Product.js
@@ -5,6 +5,9 @@ const productSchema = new mongoose.Schema({
   price: { type: String, required: true },
   image: { type: String, required: true },
   description: { type: String, required: true },
+  rate: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Rate' }],
+  gallery: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Gallery' }],
+  category: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }],
 });
 
 module.exports = mongoose.model('Product', productSchema);
