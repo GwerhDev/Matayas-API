b62150d78f7df2815f5ebd8a303e91b61e2e12a4
GwerhDev <gwerh.dev@gmail.com>
2023-11-06 16:49:23 -0300

    gallery endpoints

--- src/models/Gallery.js
+++ src/models/Gallery.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const gallerySchema = new mongoose.Schema({
   file: { type: String, required: true },
-  name: { type: String, required: false },
+  title: { type: String, required: false },
   description: { type: String, required: false },
 });
 
