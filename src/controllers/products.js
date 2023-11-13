e027dcec8bb3d9eb9d2ae51e8cc6bc450100daf1
GwerhDev <gwerh.dev@gmail.com>
2023-11-13 12:02:44 -0300

    last products endpoints

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
 const productSchema = require('../models/Product');
-const productGallerySchema = require('../models/ProductGallery');
 
 router.get('/', async (req, res) => {
   try {
     const response = await productSchema.find();
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({ error: error });
+  }
+});
+
+router.get('/lasts', async (req, res) => {
+  try {
+    const response = await productSchema.find().sort({ _id: -1 }).limit(3);
     return res.status(200).json(response);
   } catch (error) {
     return res.status(500).json({ error: error });
